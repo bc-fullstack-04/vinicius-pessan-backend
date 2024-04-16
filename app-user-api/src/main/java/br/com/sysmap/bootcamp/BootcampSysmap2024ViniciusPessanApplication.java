9207b74fa71698bd7a7ec072bfec9f39639eaf3d
Vinicius Pessan <136939544+ViniPessan@users.noreply.github.com>
2024-04-16 20:06:44 -0300

    Initial commit

---
+++ app-user-api/src/main/java/br/com/sysmap/bootcamp/BootcampSysmap2024ViniciusPessanApplication.java
@@ -0,0 +1,13 @@
+package br.com.sysmap.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSysmap2024ViniciusPessanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSysmap2024ViniciusPessanApplication.class, args);
+	}
+
+}
